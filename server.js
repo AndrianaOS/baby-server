01728895c91a61355df2823b5f5b20a199342fca
Andriana <andrianasaffo@gmail.com>
2023-06-19 14:21:40 +0100

    Include sql queries to server

--- server.js
+++ server.js
@@ -16,45 +16,92 @@ app.get("/", (req, res) => {
   res.send("Andriana's Baby name server");
 });
 
-// SEARCH NAMES
+// GET AND SEARCH NAMES
 app.get("/names", (req, res) => {
   const searchName = req.query.name || "";
-  //   console.log(searchName);
 
-  let nameResults = names.filter((eachName) => {
-    // console.log(eachName.name.toLowerCase());
-    return eachName.name.toLowerCase().includes(searchName.toLowerCase());
-  });
-  //   console.log(nameResults);
-  res.status(200).json(nameResults);
+  let getQuery = "SELECT * FROM names";
+
+  const searchQuery = " WHERE lower(name) LIKE $1 || '%'";
+
+  names
+    .query(getQuery + searchQuery, [searchName])
+    .then((result) => {
+      if (result.rowCount === 0) {
+        res.status(400).json({ error: "Names does not exist in this list" });
+      } else {
+        console.log(result.rows);
+        const allResults = result.rows.filter((eachName) => {
+          return eachName.name.toLowerCase().includes(searchName.toLowerCase());
+        });
+        return res.json(allResults);
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(400).send(err.error);
+    });
 });
 
 // POST NAMES
 app.post("/name", (req, res) => {
-  const { name, sex } = req.body;
+  // const { name, sex } = req.body;
 
-  const newID = names.length + 20;
-  //   console.log(newID);
+  const newName = req.body.name;
+  const newSex = req.body.sex;
 
-  const newName = {
-    id: newID,
-    name,
-    sex,
-  };
+  const postNameQuery = "INSERT INTO names (name, sex) VALUES ($1, $2)";
+  const getNamesQuery = "SELECT 1 FROM names WHERE name = $1";
 
-  if (!newName.name || !newName.sex) {
-    return res.status(400).json({ error: "Please fill all fields" });
-  }
+  names
+    .query(getNamesQuery, [newName])
+    .then((result) => {
+      if (result.rowCount > 0) {
+        throw { error: "Name already exists" };
+      } else if (!newName || !newSex) {
+        throw { error: "Please fill in all fields" };
+      } else {
+        return names.query(postNameQuery, [newName, newSex]);
+      }
+    })
+    .then((result) => {
+      console.log(result.rows);
+      res.status(200).json({ message: "New name added" });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json(err.error);
+    });
 
-  for (let eachName of names) {
-    // console.log(eachName.name);
-    if (eachName.name === newName.name) {
-      return res.status(400).json({ error: "Name already exists" });
-    }
-  }
+  // if(nameResults.rowCount > 0) {
 
-  names.push(newName);
-  res.status(200).json({ message: "New name added", newName, names });
+  // } else if (!newName || !newSex) {
+  //   throw {error: "Please fill in all fields"}
+  // }
+
+  // res.status(200).json({ message: "New name added" });
+  // const newID = names.length + 20;
+  // //   console.log(newID);
+
+  // const newName = {
+  //   id: newID,
+  //   name,
+  //   sex,
+  // };
+
+  // if (!newName.name || !newName.sex) {
+  //   return res.status(400).json({ error: "Please fill all fields" });
+  // }
+
+  // for (let eachName of names) {
+  //   // console.log(eachName.name);
+  //   if (eachName.name === newName.name) {
+  //     return res.status(400).json({ error: "Name already exists" });
+  //   }
+  // }
+
+  // names.push(newName);
+  // res.status(200).json({ message: "New name added", newName, names });
 });
 
 app.listen(port, () => {
